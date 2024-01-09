6fc2b1cb2cb6721661a7dfc35e24a22b7d4d599a
smadeira22 <saramariaramosmadeira@gmail.com>
2024-01-09 17:25:02 +0000

    edit structure

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -4,27 +4,29 @@ import './index.css'
 export const HomePage = () => {
   return (
     <>
+    <section id="page">
     <section id="top">
       <div class="message">
-        <div>
           <h1>Level Up your coding game</h1>
           <p>Play with your friends</p>
           <p>Use your favourite programming language</p>
-        </div>
-        <img src="https://i.ytimg.com/vi/I7YYJObe5SU/maxresdefault.jpg" alt="initialImage"></img>
       </div>
     </section>
     <section id="middle">
       <h1>Games</h1>
         <div class="filters">
-          <p>Top Games</p>
-          <p>MongoDB</p> 
-          <button>Show all</button>
+            <p>Top Games</p>
+            <p>MongoDB</p>
+            <p id="showAll">Show All</p> 
         </div>
         <div class="games">
           <img src="https://images.freecreatives.com/wp-content/uploads/2015/04/amazing-video-game-art-wallpapers-sci-fi-videogames-game.png" alt="image"/>
+          <img src="https://images.freecreatives.com/wp-content/uploads/2015/04/amazing-video-game-art-wallpapers-sci-fi-videogames-game.png" alt="image"/>
+          <img src="https://images.freecreatives.com/wp-content/uploads/2015/04/amazing-video-game-art-wallpapers-sci-fi-videogames-game.png" alt="image"/>
+          <img src="https://images.freecreatives.com/wp-content/uploads/2015/04/amazing-video-game-art-wallpapers-sci-fi-videogames-game.png" alt="image"/>
         </div>
       </section>
+    </section>
     </>
   )
 }
