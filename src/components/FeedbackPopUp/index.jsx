89d1725c4e0a2a56f55f90158c94997c79f524b7
AleFin95 <finestauri.alessia@icloud.com>
2024-01-11 22:17:29 +0000

    Fix import

--- src/components/FeedbackPopUp/index.jsx
+++ src/components/FeedbackPopUp/index.jsx
@@ -2,9 +2,6 @@ import React, { useEffect } from 'react';
 import '../../assets/App.css';
 import './index.css';
 
-
-import React from 'react'
-
 const FeedbackPopUp = () => {
   return (
     <div>Feedback Pop up window </div>
