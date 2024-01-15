d5fcf13d9357de382a7b4b5f7f5d9a29070a20c2
smadeira22 <saramariaramosmadeira@gmail.com>
2024-01-15 19:07:52 +0000

    edit structrure

--- src/components/PlayerVsPlayer/index.jsx
+++ src/components/PlayerVsPlayer/index.jsx
@@ -1,12 +1,43 @@
 import React from 'react'
 import './index.css'
 import { useState, useEffect } from 'react';
+import image from '../../assets/Versus1.png'
 
 const PlayerVsPlayer = () => {
-    const [counter, setCounter] = useState(10);
 
-    
+
+    const imageStyle = {
+        width: "350px",
+        height: "400px",
+    }
+
+    const imageOpacity = {
+        opacity: "1",
+        transition: "opacity 1s ease", /* Adjust the duration and easing function as needed */
+    }
+    const [showImage, setShowImage] = useState(false);
+    const [showImage2, setShowImage2] = useState(false);
+    const [startTimer , setStartTimer] = useState(true);
+    const [zoomOn, setZoomOn] = useState(false);
+
     useEffect(() => {
+      // Set a timeout to change the state after 2000 milliseconds (2 seconds)
+      const timeoutId = setTimeout(() => {
+        setShowImage(true);
+        setShowImage2(true);
+        setStartTimer(true);
+      }, 500);
+      
+      // Clear the timeout when the component unmounts or when the image is displayed
+      return () =>clearTimeout(timeoutId);
+                   
+    }, []); // Empty dependency array ensures the effect runs only once (on mount)
+  
+    const [counter, setCounter] = useState(10);
+ 
+   
+    useEffect(() => { 
+        if(startTimer){
         const intervalId = setInterval(() => {
           setCounter(prevCounter => {
             const newCounter = prevCounter - 1;
@@ -15,6 +46,7 @@ const PlayerVsPlayer = () => {
             if (newCounter === 0) {
               clearInterval(intervalId); // Stop the interval
               localStorage.setItem("interval", false);
+              //setZoomOn(true);
             }
     
             return newCounter;
@@ -22,8 +54,9 @@ const PlayerVsPlayer = () => {
         }, 1000);
     
         return () => clearInterval(intervalId);
-      }, []);
-
+         }
+      }, [startTimer]);
+   
     return (
         <> 
         <section id="first">
@@ -32,13 +65,17 @@ const PlayerVsPlayer = () => {
                     <img src={localStorage.getItem("selectedAvatar")}/>
                     <h1>{localStorage.getItem("username")}</h1>
                 </div> 
-                <div className="player2"> 
-                    <img src={localStorage.getItem("selectedAvatar")}/>
-                    <h1>ss</h1>
+                <div  className={`image-container ${showImage ? 'show' : ''}${zoomOn ? 'zoom-in' : ''}`}>
+                    <img src={image} alt="Displayed Image" style={imageStyle} />
                 </div>
+                <div className={`image-container2 ${showImage ? 'slide-in' : ''}`}>
+                    <img src={localStorage.getItem("selectedAvatar")}/>
+                    <h1>{localStorage.getItem("username")}</h1>
+                </div> 
             </section>
             <div className='title'>
-                <h1>The game will start in: {counter}</h1>
+                <h1>The game will start in:</h1>
+                <p>{counter}</p>
             </div>
         </section>
         </>
