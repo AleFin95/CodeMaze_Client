0242a4957ed99a0a2240324b4d6c865bc996af5d
Emma Collins <lowpee56@hotmail.com>
2024-01-15 08:19:59 +0000

    game test cases

---
+++ src/components/GameTestCases/GameTestCases.test.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { describe, it, expect, beforeEach, afterEach } from "vitest";
+import { screen, render, cleanup } from "@testing-library/react";
+import { AuthProvider } from "../../contexts/index";
+import { BrowserRouter } from "react-router-dom";
+import GamePage from "../../pages/GamePage";
+import matchers from "@testing-library/jest-dom/matchers";
+expect.extend(matchers);
+
+import GameTestCases from ".";
+
+describe("GameTestCases", () => {
+  beforeEach(() => {
+    render(
+      <AuthProvider>
+        <BrowserRouter>
+          <GamePage>
+            <GameTestCases />
+          </GamePage>
+        </BrowserRouter>
+      </AuthProvider>
+    );
+  });
+
+  it("should find the heading", () => {
+    const heading = screen.getByRole("heading", { name: /Test Cases:/i });
+    expect(heading).toBeInTheDocument();
+  });
+
+  it("should find the test cases", () => {
+    const testCaseRegex = /print\(twoSum\(\[2, 7, 11, 15\], 9\)\)/i;
+    const testCase = screen.getByText(testCaseRegex);
+    expect(testCase).toBeInTheDocument();
+  });
+
+  afterEach(() => {
+    cleanup();
+  });
+});
