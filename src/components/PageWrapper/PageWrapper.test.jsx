16d04580ce2fd6c0c4307a5f7e96856d8570f25b
Emma Collins <lowpee56@hotmail.com>
2024-01-16 17:35:25 +0000

    Refactor test page wrapper in tests

--- src/components/PageWrapper/PageWrapper.test.jsx
+++ src/components/PageWrapper/PageWrapper.test.jsx
@@ -1,72 +1,78 @@
-import React from 'react';
-import { describe, it, expect, beforeEach, afterEach } from 'vitest';
-import { screen, render, cleanup, fireEvent, waitFor, getByTestId } from '@testing-library/react';
+import React from "react";
+import { describe, it, expect, beforeEach, afterEach } from "vitest";
+import {
+  screen,
+  render,
+  cleanup,
+  fireEvent,
+  waitFor,
+  getByTestId,
+} from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
+import { BrowserRouter, MemoryRouter } from "react-router-dom";
 
-import { BrowserRouter, MemoryRouter } from 'react-router-dom';
-
-import matchers from '@testing-library/jest-dom/matchers';
+import matchers from "@testing-library/jest-dom/matchers";
 expect.extend(matchers);
-import PageWrapper from '.';
-import { AuthProvider } from '../../contexts';
+import PageWrapper from ".";
+import { AuthProvider } from "../../contexts";
 
-describe('Page Wrapper Component',() => {
-    beforeEach(() => {
+describe("Page Wrapper Component", () => {
+  beforeEach(() => {
     render(
-    <AuthProvider>
+      <AuthProvider>
         <BrowserRouter>
-            <PageWrapper />
+          <PageWrapper />
         </BrowserRouter>
-    </AuthProvider> 
-    )
+      </AuthProvider>
+    );
+  });
+  it("displays a nav with 2 children", () => {
+    const nav = screen.getByTestId("div");
+
+    expect(nav).toBeInTheDocument();
+    expect(nav.childNodes.length).toBe(2);
+  });
+
+  it("it renders Ranking and Game links", () => {
+    /*expect(screen.getByText("Home")).toBeInTheDocument();*/
+    expect(screen.getByText("Ranking")).toBeInTheDocument();
+    expect(screen.getByText("Login/Register")).toBeInTheDocument();
+  });
+
+  /*it("User is forwarded to Home page when Home link is clicked", async () => {
+    const link1 = screen.getByText("Home");
+    expect(link1).toBeInTheDocument();
+    fireEvent.click(link1);
+
+    // Wait for any asynchronous behavior, if applicable
+    await waitFor(() => {
+      expect(window.location.pathname).toBe("/");
     });
-    it('displays a nav with 3 children', () => {
-        const nav = screen.getByTestId('div')
+  }); */
 
-        expect(nav).toBeInTheDocument()
-        expect(nav.childNodes.length).toBe(3)
-    })
+  it("User is forwarded to Ranking page when Ranking link is clicked", async () => {
+    const link1 = screen.getByText("Ranking");
+    expect(link1).toBeInTheDocument();
+    fireEvent.click(link1);
 
-    it('it renders Home, Ranking and Game links', () => {
-        expect(screen.getByText('Home')).toBeInTheDocument();
-        expect(screen.getByText('Ranking')).toBeInTheDocument();
-        expect(screen.getByText('Login/Register')).toBeInTheDocument();
-    })
+    // Wait for any asynchronous behavior, if applicable
+    await waitFor(() => {
+      expect(window.location.pathname).toBe("/ranking");
+    });
+  });
 
-    it('User is forwarded to Home page when Home link is clicked', async() => {
-        const link1 = screen.getByText('Home')
-        expect(link1).toBeInTheDocument()
-        fireEvent.click(link1);
-      
-        // Wait for any asynchronous behavior, if applicable
-        await waitFor(() => {
-          expect(window.location.pathname).toBe('/');
-        });
-    })
+  it("User is forwarded to Login page when Login link is clicked", async () => {
+    const link1 = screen.getByText("Login/Register");
+    expect(link1).toBeInTheDocument();
+    fireEvent.click(link1);
 
-    it('User is forwarded to Ranking page when Ranking link is clicked', async() => {
-        const link1 = screen.getByText('Ranking')
-        expect(link1).toBeInTheDocument()
-        fireEvent.click(link1);
-      
-        // Wait for any asynchronous behavior, if applicable
-        await waitFor(() => {
-          expect(window.location.pathname).toBe('/ranking');
-        });
-    })
-    
-    it('User is forwarded to Login page when Login link is clicked', async() => {
-        const link1 = screen.getByText('Login/Register')
-        expect(link1).toBeInTheDocument()
-        fireEvent.click(link1);
-      
-        // Wait for any asynchronous behavior, if applicable
-        await waitFor(() => {
-          expect(window.location.pathname).toBe('/login');
-        });
-    })
-    /*
+    // Wait for any asynchronous behavior, if applicable
+    await waitFor(() => {
+      expect(window.location.pathname).toBe("/login");
+    });
+  });
+  /*
     it('User is forwarded to Profile page when Avatar is clicked', async() => {
       const link1 = screen.getByRole('img')
       expect(link1).toBeInTheDocument()
@@ -90,9 +96,7 @@ describe('Page Wrapper Component',() => {
     
   })*/
 
-  
-
-    /*
+  /*
     it( 'dsds', async() => {
          const rankingLink = screen.getByText((content, element) => {
         // Check if the text "Ranking" is present in any of the descendant elements
@@ -111,8 +115,7 @@ describe('Page Wrapper Component',() => {
    
 })*/
 
-
-afterEach(() => {
-    cleanup()
-  })
-})
+  afterEach(() => {
+    cleanup();
+  });
+});
