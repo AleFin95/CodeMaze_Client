89bfdaca02aee83810911c7cbfdd7beda1105f05
Emma Collins <lowpee56@hotmail.com>
2024-01-16 18:51:47 +0000

    Refactor test game questions

--- src/components/FeedbackPopUp/index.jsx
+++ src/components/FeedbackPopUp/index.jsx
@@ -1,17 +1,9 @@
-import React, { useEffect } from 'react';
-import '../../assets/App.css';
-import './index.css';
+import React, { useEffect } from "react";
+import "../../assets/App.css";
+import "./index.css";
 
 const FeedbackPopUp = ({ buttonPressed }) => {
-  return (
-    <div>
-      {buttonPressed ? (
-        <h1>You WIN</h1>
-      ) : (
-        <h1>You LOOSE</h1>
-      )}
-    </div>
-  )
-}
+  return <div>{buttonPressed ? <h1>You WIN</h1> : <h1>You LOOSE</h1>}</div>;
+};
 
-export default FeedbackPopUp
+export default FeedbackPopUp;
--- src/components/GameQuestions/index.jsx
+++ src/components/GameQuestions/index.jsx
@@ -1,47 +1,9 @@
-import { useEffect, useState } from "react";
-import { Axios } from "axios";
-
-const GameQuestions = ({ socket, room, roomData, name, isSolo, initialQ }) => {
-  const [userRooms, setUserRooms] = useState();
-  const [question, setQuestion] = useState(initialQ);
-  //replace text initial state with the random question
-
-  const [text, setText] = useState(initialQ);
-
-  // const [text, setText] = useState(
-  //   `question number is ${Math.random() * 10}`
-  // );
-
-  console.log("question room: ", room);
-  console.log("question roomData: ", roomData);
-  console.log("qNmae: ", name);
-  console.log("isSolo: ", isSolo);
-  console.log("Question: ", question);
-
-  useEffect(() => {
-    if (!isSolo) {
-      socket.emit("send_question", { initialQ, room, name });
-
-      const handleGetQuestion = (data) => {
-        console.log("data: ", data);
-        setQuestion(data);
-      };
-
-      socket.on("get_question", handleGetQuestion);
-
-      return () => {
-        socket.off("get_question", handleGetQuestion);
-      };
-    } else if (isSolo) {
-      setQuestion(initialQ);
-    }
-  }, [socket, initialQ]);
-
+const GameQuestions = ({ initialQ }) => {
   return (
     <>
       <h4>Question:</h4>
       <div className="input-box">
-        <textarea readOnly id="code-inp" value={question}></textarea>
+        <textarea readOnly id="code-inp" value={initialQ}></textarea>
       </div>
     </>
   );
