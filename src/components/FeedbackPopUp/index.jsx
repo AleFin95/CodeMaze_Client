7a18bad8fe1b6dfdb0bfdfab9b9927eb5ea94677
jarvis <jsd.ditcher@outlook.com>
2024-01-17 14:18:45 +0000

    commit

--- src/components/FeedbackPopUp/index.jsx
+++ src/components/FeedbackPopUp/index.jsx
@@ -2,13 +2,26 @@ import React, { useEffect } from 'react';
 import '../../assets/App.css';
 import './index.css';
 
-const FeedbackPopUp = ({ buttonPressed }) => {
+const FeedbackPopUp = ({ buttonPressed, expectedOutcome, correctAnswer }) => {
   return (
     <div>
-      {buttonPressed ? (
-        <h1>You WIN</h1>
+      { buttonPressed ? (
+        <>
+          { correctAnswer ? (
+              <h1>You WIN</h1>
+            ) : (
+              <h1>False Alert, keep going, BUTTON PRESS</h1>
+            )}
+        </>
       ) : (
-        <h1>You LOOSE</h1>
+        correctAnswer ? (
+          <>
+          <h1>You LOOSE</h1>
+          <p>CORRECT ANSWER: {expectedOutcome}</p>
+          </>
+        ) : (
+          <h1>False Alert, keep going</h1>
+        )
       )}
     </div>
   )
