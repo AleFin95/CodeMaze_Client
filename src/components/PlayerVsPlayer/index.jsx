2f8e63be69c2a511f4e7244838304edc69066247
nine96as <ashrafsaid79@gmail.com>
2024-01-18 10:00:01 +0000

    Fix avatar rendering issue

--- src/components/PlayerVsPlayer/index.jsx
+++ src/components/PlayerVsPlayer/index.jsx
@@ -1,8 +1,7 @@
-import React from "react";
-import "./index.css";
-import { useState, useEffect } from "react";
-import { VideoVs } from "../../components";
 import 'animate.css';
+import React, { useEffect, useState } from 'react';
+import { VideoVs } from '../../components';
+import './index.css';
 
 const PlayerVsPlayer = ({ roomUsers2, onTimeOut }) => {
   const [counter, setCounter] = useState(10);
@@ -10,10 +9,8 @@ const PlayerVsPlayer = ({ roomUsers2, onTimeOut }) => {
   const [secondAnimation, setSecondAnimation] = useState(false);
   const [timerShow, setTimerShow] = useState(false);
   const [newds, setNewds] = useState(true);
-  const [enemyName, setEnemyName] = useState();
-
-  /* localStorage.removeItem("interval")
-    localStorage.setItem("interval", true);*/
+  const [enemyName, setEnemyName] = useState('');
+  const [enemyAvatar, setEnemyAvatar] = useState('');
 
   useEffect(() => {
     // Function to be executed after the component has mounted
@@ -22,14 +19,55 @@ const PlayerVsPlayer = ({ roomUsers2, onTimeOut }) => {
       setSecondAnimation(true);
     }, 1800); // Set the delay duration (in milliseconds)
 
-     
+    const fetchUsersAvatarData = async (username, enemyName) => {
+      try {
+        const options = {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            username_one: username,
+            username_two: enemyName
+          })
+        };
+        const accessToken = localStorage.getItem('access_token');
+        if (accessToken) {
+          options.headers['Authorization'] = `Bearer ${accessToken}`;
+        }
 
-    console.log("roomUsers2: ", roomUsers2);
+        console.log(
+          `FETCH USER DATA username: ${username}, enemyname: ${enemyName}`
+        );
+
+        const response = await fetch(
+          'https://codemaze-api.onrender.com/users/avatars',
+          options
+        );
+
+        if (response.status === 200) {
+          const data = await response.json();
+          console.log(data);
+          if (data[0].username !== username) {
+            setEnemyAvatar(data[0].avatar);
+          } else {
+            setEnemyAvatar(data[1].avatar);
+          }
+        } else {
+          throw new Error('Failed to fetch avatar');
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    console.log('roomUsers2: ', roomUsers2);
 
     for (let i = 0; i < roomUsers2.length; i++) {
       const currentName = roomUsers2[i];
       if (currentName !== username) {
         setEnemyName(currentName);
+        fetchUsersAvatarData(username, currentName);
       }
     }
 
@@ -53,7 +91,7 @@ const PlayerVsPlayer = ({ roomUsers2, onTimeOut }) => {
             // Check if the counter reaches 0
             if (newCounter === 1) {
               clearInterval(intervalId); // Stop the interval
-              localStorage.setItem("interval", false);
+              localStorage.setItem('interval', false);
 
               onTimeOut();
             }
@@ -70,88 +108,36 @@ const PlayerVsPlayer = ({ roomUsers2, onTimeOut }) => {
       };
     }
 
-    localStorage.removeItem("interval");
+    localStorage.removeItem('interval');
   }, [
     secondAnimation,
     setSecondAnimation,
     setCounter,
     timerShow,
-    setTimerShow,
+    setTimerShow
   ]);
 
-  /*  useEffect(() => {
-        
-         // Set the delay duration (in milliseconds)
-    
-        // Clean up the timeout when the component is unmounted or when the div is displayed
-        return () => clearTimeout(timeoutId);
-      }, [secondAnimation]); // The empty dependency array [] ensures the effect runs only once after mount
-    */
-  /*  if(newds === false){
-          localStorage.removeItem("interval")
-        }*/
-  //const stop = localStorage.getItem("interval");
-  /*console.log("ss",stop)
-      console.log(roomData)*/
-      const username = localStorage.getItem("username");
-      //const myAvatar = //username;
-      //const enemyAvatar = ;
+  const username = localStorage.getItem('username');
 
-      useEffect(() => {
-        const fetchUsersAvatarData = async () => {
-          try {
-            const access_token = localStorage.getItem("access_token");
-            const options = {
-              method: "POST",
-              body: JSON.stringify({
-                username_one: username,
-                username_two: enemyName
-              }),
-
-              headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${access_token}`,
-              },
-            };
-    
-            const response = await fetch(
-              "https://codemaze-api.onrender.com/users/avatars",
-              options
-            );
-              
-            if (response.status === 200) {
-              const data = await response.json();
-              setData(data);
-              console.log("IM HEREEEEEE");
-              console.log("DATAAA",data);
-            } else {
-              throw new Error("Failed to fetch avatar");
-            }
-          } catch (error) {
-            console.error(error);
-          }
-        }; fetchUsersAvatarData();
-      }, []);
-      
-    return (
-        <>
-        <VideoVs />
-      <section id="first">
-      <section id="main">
-    <div className={`player1 ${secondAnimation ? "show" : ""}`}>
-      <img src={localStorage.getItem("selectedAvatar")} />
-      <h1>{localStorage.getItem("username")}</h1>
-    </div>
-    <div className={`player2 ${secondAnimation ? "show" : ""}`}>
-      <img src={localStorage.getItem("selectedAvatar")} />
-      <h1>{enemyName}</h1>
-    </div>
-  </section>
-  <div className={`title ${timerShow ? "show" : ""}`}>
-    <h1>Starting in </h1>
-    <p>{counter}</p>
-  </div>
-</section>
+  return (
+    <>
+      <VideoVs />
+      <section id='first'>
+        <section id='main'>
+          <div className={`player1 ${secondAnimation ? 'show' : ''}`}>
+            <img src={localStorage.getItem('selectedAvatar')} />
+            <h1>{localStorage.getItem('username')}</h1>
+          </div>
+          <div className={`player2 ${secondAnimation ? 'show' : ''}`}>
+            <img src={enemyAvatar} />
+            <h1>{enemyName}</h1>
+          </div>
+        </section>
+        <div className={`title ${timerShow ? 'show' : ''}`}>
+          <h1>Starting in </h1>
+          <p>{counter}</p>
+        </div>
+      </section>
     </>
   );
 };
